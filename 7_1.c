dd612fd660f1926ea0b2275f50546a5d38ab9dd8
songhee <20150261sh@gmail.com>
2017-05-17 15:11:13 +0900

    add code1

---
+++ 7_1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int grade[10];
+    int i, j, sum, tmp;
+    float avg;
+
+    for(i = 0; i < 10; i++) {
+        printf("%d 번째 성적을 입력하세요: ", i);
+        scanf("%d", &grade[i]);
+    }
+
+    for(i = sum = 0; i < 10; i++) 
+        sum += grade[i];
+    avg = sum / 10.0;
+    printf("성적 평균: %f\n", avg);
+
+    for(i = 0; i < 9; i++)
+        for(j = 9; j > i; j--)
+            if(grade[j - 1] > grade[j]) {
+                    tmp = grade[j - 1];
+                    grade[j - 1] = grade[j];
+                    grade[j] = tmp;
+            }
+
+    printf("성적순: ");
+    for(i = 0; i < 10; i++)
+        printf("%d ", grade[i]);
+    printf("\n");
+
+        return 0;
+}
